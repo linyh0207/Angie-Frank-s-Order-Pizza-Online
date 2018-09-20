991aba12168f2d15f1a8884549144309b2a39d73
bguids91 <bguids91@gmail.com>
2018-09-20 19:28:52 +0000

    Adds front end jquery

--- public/scripts/frontend-app.js
+++ public/scripts/frontend-app.js
@@ -1,11 +1,41 @@
-//Customer EJS
+//Index EJS
 $(() => {
-  function createPizzaCard() {
-    let $card = $('<div>').addClass();
+  function createPizzaCardRead() {
+    let $pizzacolumn = $('div').addClass('col-4')
+    let $pizzacard = $('<div>').addClass();
     let $pizzaimg = $('<img>').addClass().attr('src', );
     let $pizzatitle = $('<h5>').addClass();
     let $pizzadescription = $('<p>').addClass();
     let $pizzaprice = $('<h4>').addClass();
-    let $quantitybox = $('<form>')
+
+    $pizzacard.append($pizzaimg, $pizzatitle, $pizzadescription, $pizzaprice);
+    $pizzacolumn.append($pizzacard);
+
+    return $pizzacolumn
+
+});
+
+//Customer EJS
+$(() => {
+
+  function createPizzaCardAdd() {
+    let $pizzacolumn = $('div').addClass('col-4')
+    let $pizzacard = $('<div>').addClass();
+    let $pizzaimg = $('<img>').addClass().attr('src', );
+    let $pizzatitle = $('<h5>').addClass();
+    let $pizzadescription = $('<p>').addClass();
+    let $pizzaprice = $('<h4>').addClass();
+    let $quantityform = $('<form>').addClass('form-inline');
+    let $quantitytitle = $('<span>').text('Quantity');
+    let $quatityinput = $('<input>').addType('text');
+    let $quantitybutton = $('<button>').addClass('btn btn-outline-secondary').addType('submit').text('Add to Order');
+
+    $quantityform.append($quantitytitle, $quatityinput, $quantitybutton);
+    $pizzacard.append($pizzaimg, $pizzatitle, $pizzadescription, $pizzaprice, $quantityform);
+    $pizzacolumn.append($pizzacard);
+
+    return $pizzacolumn
   }
+
+  function createCartCard
 });
