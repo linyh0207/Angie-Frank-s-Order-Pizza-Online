57f5479079a52d03cdc0b61d9c8e9689046479cd
Frank Chung <ponty33@gmail.com>
2018-09-20 02:54:09 +0000

    adds database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,27 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+// $(function () {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done((users) => {
+//     for(user of users) {
+//       $("<div>").text(user.name).appendTo($("body"));
+//     }
+//   });;
+// });
+
+$(function () {
+  //id NOT set yet
+  //For checkout button
+  let $button = $('#checkoutbutton')
+  $button.click(function (e) {
+    e.preventDefault();
+    // NEED /checkout page in html!
+    $.ajax({
+      method: "GET",
+      url: "/checkout",
+      success: function (res) {
+        alert('SUCCESS');
+      }
+    })
+  });
 });
