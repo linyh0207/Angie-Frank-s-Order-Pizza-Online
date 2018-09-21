3034bc426430da0f7bf2f044e0e68c477f13630a
Frank Chung <ponty33@gmail.com>
2018-09-21 03:56:07 +0000

    add cart_function

--- public/scripts/checkout.js
+++ public/scripts/checkout.js
@@ -16,14 +16,6 @@
       //   }
       // })
     });
-    const fakeMenu = [
-      {
-        name: 'abc', 
-        price: 5.99,
-        description: 'GOOOOD',
-        url: 'fdfdfdfdf'
-      }
-    ]
 
 
 
@@ -33,13 +25,16 @@
     const createMenu = function (item) {
       let $item = $('<div>').addClass('card');
       let $img = $('<img>').addClass('card-image-top').attr('src',item.url);
-      let $name = $('<h5>').addClass('card-title').text(item.name);
+      let $name = $('<h5>').addClass('card-title').text(item.pizza_name);
       let $des = $('<p>').addClass('card-text').text(item.description);
       let $price = $('<h4>').text(item.price);
       let $form = $('<form>').addClass('form-inline');
       let $quantity = $('<span>').text('Quantity');
-      let $input = $('<input>').attr('type', 'text');
-      let $add = $('<button>').addClass('btn btn-outline-secondary').attr('type', 'submit').text('Add to Order');
+      let $input = $('<input>').attr('type', 'text').attr('data-add-quantity', item.pizza_name);
+      let $add = $('<button>').addClass('btn btn-outline-secondary').attr('type', 'submit')
+      .attr('data-add-pizza', item.pizza_name)
+      .attr('data-add-price', item.price)
+      .text('Add to Order');
     
       $item.append($img, $name, $des, $price, $form);
       $form.append($quantity, $input, $add);
@@ -54,26 +49,40 @@
       })
     }
 
-    renderMenu(fakeMenu);
+    const loadMenu = function () {
+      $.ajax('/customer/menu', { method: 'GET' })
+      .then(function (pizzas) {
+        renderMenu(pizzas);
+      })
+    }
+
+    loadMenu();
+
+    let $menuContainer = $('#menuContainer');
+    $menuContainer.on('click', '*[data-add-pizza]', function(event) {
+      let $this = $(this);
+
+      let quant = $this.siblings('input[data-add-quantity]').val();
+      
+      let $cart = $('<div>').addClass('card-body')
+      let $cartName = $('<h5>').addClass('card-title').text($this.data('add-pizza'));
+      let $cartQuan = $('<h5>').addClass('card-subtitle').text(`Quantity: ${quant}`);
+      let $cartPrice = $('<h6>').addClass('card-subtitle')
+      .text('$ '+ ($this.data('add-price')) * quant );
+      
+      $cart.append($cartName, $cartQuan, $cartPrice)
+      $('#cartContainer').append($cart);
+
+   
+
+
+
+
+      return false;
+    })
+
 
 
   });
 
 
-
-
-
-
-
-// function makeLanguageCard(language){
-//   var $card = $(`<div class="col-4 col-sm-6 col-xs-12 language">
-//   <div class="card">
-//     <img class="card-img-top" src="${language.imgUrl}" alt="${language.name}">
-//     <div class="card-body">
-//       <h5 class="card-title">${language.name}</h5>
-//       ${language.description.split('\n').map(para => `<p class="card-text">${para}</p>`).join('')}
-//       <a href="#" class="btn btn-primary" data-alert-name="${language.name}">Go somewhere</a>
-//       <a href="#" class="btn btn-danger" data-delete-id="${language.id}">Delete this</a>
-//     </div>
-//   </div>    
-// </div>`);
