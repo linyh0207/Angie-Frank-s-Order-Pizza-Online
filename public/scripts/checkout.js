2ab2b3a78ad0b0a202bd25f10d9efc4325aafbb0
Angela <liny0207@gmail.com>
2018-09-22 07:10:07 +0000

    organizes the checkout.js file

--- public/scripts/checkout.js
+++ public/scripts/checkout.js
@@ -1,31 +1,11 @@
 $(function () {
-  //id NOT set yet
-  //For checkout button
-    let $button = $('#checkoutbtn')
-    $button.on('submit', function (e) {
-      e.preventDefault();
-      $('#status').removeAttr('hidden');
-      // NEED /checkout page in html!
-      // $.ajax({
-      //   method: "GET",
-      //   url: "/checkout",
-      //   success: function (res) {
-      //     alert('SUCCESS');
-      //   }
-      // })
-
-      // NEED error mesage for empty shopping cart
-    });
-
+    //Create, Render and Load Menu Items -- Start
     const createMenu = function (item) {
       let $item = $('<div>').addClass('card');
       let $img = $('<img>').addClass('card-image-top').attr('src',item.url);
       let $name = $('<p>').addClass('card-title').text(item.pizza_name);
       let $des = $('<p>').addClass('card-text').text(item.description);
       let $price = $('<h4>').text(item.price);
-      //need to add class for subtotal 
-      //maybe separate the quantity to easy reference
-      //handle zero ihnput for quantity
       let $form = $('<form>').addClass('form-inline');
       let $quantity = $('<span>').text('Quantity');
       let $input = $('<input>').attr('type', 'text').attr('data-add-quantity', item.pizza_name);
@@ -56,8 +36,29 @@ $(function () {
     }
 
     loadMenu();
+    //Create, Render and Load Menu Items -- End
 
+
+    //Create and Render Shopping Cart -- Start
+    const createCartElement = function (shoppingItem){
+        let $cart = $('<div>').addClass('card-body')
+        let $cartName = $('<h5>').addClass('card-title').text(shoppingItem.pizza_name);
+        let $cartQuan = $('<h5>').addClass('card-subtitle').text(shoppingItem.qty);
+        let $cartPrice = $('<h6>').addClass('card-subtitle').text(shoppingItem.sub_total);
+        $cart.append($cartName, $cartQuan, $cartPrice);
+        return $cart;
+    }
+    const renderCart = function (shoppingItems) {
+        shoppingItems.forEach(function(shoppingItem){
+        $('#cartContainer').append(createCartElement(shoppingItem)); 
+        })
+    }
+    // Create and Render Shopping Cart -- Ends
+
+
+    //Add a item to shopping cart when valid quantity enter and click the add button-- Start
     let $menuContainer = $('#menuContainer');
+    
     $menuContainer.on('click', '*[data-add-pizza]', function(event) {
         event.preventDefault();
     
@@ -70,25 +71,7 @@ $(function () {
             'qty': quant,
             'sub_total': $subTotal
         }
-        
-        const createCartElement = function (shoppingItem){
-            let $cart = $('<div>').addClass('card-body')
-            let $cartName = $('<h5>').addClass('card-title').text(shoppingItem.pizza_name);
-            let $cartQuan = $('<h5>').addClass('card-subtitle').text(shoppingItem.qty);
-            let $cartPrice = $('<h6>').addClass('card-subtitle').text(shoppingItem.sub_total);
-            $cart.append($cartName, $cartQuan, $cartPrice)
-            return $cart;
-        }
-
-        const renderCart = function (shoppingItems) {
-            console.log(shoppingItems);
-            shoppingItems.forEach(function(shoppingItem){
-
-                $('#cartContainer').append(createCartElement(shoppingItem));
-                console.log(shoppingItem);
-            })
-        }
-
+    
         if(!quant || quant === '0'){
             errorMsg.removeAttr('hidden').text('invalid quantity');   
                 } else {
@@ -101,52 +84,34 @@ $(function () {
                         $this.siblings('input[data-add-quantity]').val('');
                         return $.ajax('/customer/cart');
                     }).then(renderCart);
-                    
-                    // .catch((err) => {
-                    //     console.log('err',err)
-                    // })
         };
-
-      
     });
+    //Add a item to shopping cart when valid quantity enter and click the add button -- End
 
+    //Load shopping cart -- Start
     const loadCart = function (){
         $.ajax('/customer/cart', {method: 'GET'})
         .then(function(shoppintItems){
             renderCart(shoppingItems);
-            // for (item of carts){
-            //     let $cart = $('<div>').addClass('card-body')
-            //     let $cartName = $('<h5>').addClass('card-title').text(item.pizza_name);
-            //     let $cartQuan = $('<h5>').addClass('card-subtitle').text(item.qty);
-            //     let $cartPrice = $('<h6>').addClass('card-subtitle').text(item.sub_total);
-            //     $('#cartContainer').append($cart);
-            //     $cart.append($cartName, $cartQuan, $cartPrice)
-            // };
         }); 
     };
     loadCart();
+    //Load shopping cart -- End
 
+
+    //Expand checkout details when click checkout button -- Start
+    let $button = $('#checkoutbtn')
+    $button.on('submit', function (e) {
+      e.preventDefault();
+      $('#status').removeAttr('hidden');
+      // $.ajax({
+      //   method: "GET",
+      //   url: "/checkout",
+      //   success: function (res) {
+      //     alert('SUCCESS');
+      //   }
+      // })
+      // Will need to set up error mesage for empty shopping cart
+    });
+    //Expand checkout details when click checkout button -- End
 });
-
-
-
-
-
-
-// function renderCart(items){
-//     for (item of carts){
-//         $('#cartContainer').append(createCartElement(item));
-//     }
-// }
-
-
-// function createCartElement(item){
-//     let $cart = $('<div>').addClass('card-body')
-//     let $cartName = $('<h5>').addClass('card-title').text(item.pizza_name);
-//     let $cartQuan = $('<h5>').addClass('card-subtitle').text(item.qty);
-//     let $cartPrice = $('<h6>').addClass('card-subtitle').text(item.sub_total);
-//     $cart.append($cartName, $cartQuan, $cartPrice)
-//     return $cart;
-// }
-
-
